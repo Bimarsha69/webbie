c1fae3354ce1fc979c93c04b7574d0fd0c8a0237
Bimarsha69 <116154292+Bimarsha69@users.noreply.github.com>
2024-11-16 15:02:16 +0545

    Create home.tsx

---
+++ home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto mt-8 p-4">
+      <h1 className="text-4xl font-bold mb-4">Bimarsha</h1>
+      <p className="text-xl">
+        Bimarsha's Website.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
